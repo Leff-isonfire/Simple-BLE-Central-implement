b2a67444927465ec8ade8682e4df4b4be14177a7
Leff-isonfire <Jason@JasonPandeMacBook-Pro.local>
2015-07-13 15:09:29 +0800

    Initial Commit

---
+++ Mr_2C/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mr_2C
+//
+//  Created by Leff on 15/7/13.
+//  Copyright (c) 2015年 LeffPan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
