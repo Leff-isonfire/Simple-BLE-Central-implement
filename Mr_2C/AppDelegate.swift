15fa69cc0e695f9752fa3f4f5071f0ef5b5946eb
Leff-isonfire <Jason@JasonPandeMacBook-Pro.local>
2015-08-06 09:40:14 +0800

    Work well until connect to peripheral.
    scanPeripheralForService and discover specify device, unable to connect
    to it.

--- Mr_2C/AppDelegate.swift
+++ Mr_2C/AppDelegate.swift
@@ -7,38 +7,102 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CBPeripheralDelegate, CBCentralManagerDelegate {
 
     var window: UIWindow?
+    
+    //设备的唯一识别码 —— Set device's UUID
+    let deviceUUID = UIDevice.currentDevice().identifierForVendor
+    //服务的UUID —— Set service's UUID
+    let kServiceUUID:String = "C4FB2349-72FE-4CA2-94D6-1F3CB16222AA"
+    //特征的UUID —— Set characteristic's UUID
+    let kCharacteristicUUID_1:String = "6A3E4B28-522D-4B3B-82A9-D5E2004534FA"
+    let kCharacteristicUUID_2:String = "6A3E4B28-522D-4B3B-82A9-D5E2004534FB"
+    let kCharacteristicUUID_3:String = "6A3E4B28-522D-4B3B-82A9-D5E2004534FC"
 
-
+    var centralManager:CBCentralManager!
+    var connectingPeripheral = CBPeripheral()
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        self.centralManager = CBCentralManager.init(delegate: self, queue: nil, options:[CBCentralManagerOptionRestoreIdentifierKey:"centralRestoreKey"])
+        
         return true
     }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        
+        centralManager.scanForPeripheralsWithServices([ transformStringToCBUUID(kServiceUUID) ], options: nil)
+        
+    }
+    
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        
+        println(peripheral.name)
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        self.centralManager.connectPeripheral(peripheral, options: [CBConnectPeripheralOptionNotifyOnConnectionKey:"peripheralOptionNotifyOnConnectionKey"])
+        
+
+        
+    }
+    
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        
+//        let characteristic1:CBUUID = CBUUID.init(string: kCharacteristicUUID_1)
+//        let characteristic2:CBUUID = CBUUID.init(string: kCharacteristicUUID_2)
+//        let characteristic3:CBUUID = CBUUID.init(string: kCharacteristicUUID_3)
+//        
+//        var characteristicArray:[CBUUID] = [characteristic1,characteristic2,characteristic3]
+        
+//        peripheral.discoverCharacteristics(characteristicArray, forService:peripheral.services.first as! CBService )
+        peripheral.delegate = self
+        peripheral.discoverServices([ transformStringToCBUUID(kServiceUUID) ])
+        
+    }
+    
+    func centralManager(central: CBCentralManager!, didFailToConnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
+        println(error.localizedDescription)
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        let characteristic1:CBUUID = CBUUID.init(string: kCharacteristicUUID_1)
+        let characteristic2:CBUUID = CBUUID.init(string: kCharacteristicUUID_2)
+        let characteristic3:CBUUID = CBUUID.init(string: kCharacteristicUUID_3)
+        
+        var characteristicArray:[CBUUID] = [characteristic1,characteristic2,characteristic3]
+        
+//        CBService.init(UUID: transformStringToCBUUID(kServiceUUID), primary: true)
+        
+        peripheral.discoverCharacteristics(characteristicArray, forService:peripheral.services.first as? CBService )
+        
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+        
+        var characteristicsToBeRead: [CBCharacteristic] = service.characteristics as! [CBCharacteristic]
+        
+        for characteristicItem in characteristicsToBeRead {
+            peripheral.readValueForCharacteristic(characteristicItem)
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didUpdateValueForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
+        
+        var datastring = NSString(data: characteristic.value!, encoding: NSUTF8StringEncoding)
+        var ssstring:String = datastring as! String
+        println(ssstring)
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func centralManager(central: CBCentralManager!, willRestoreState dict: [NSObject : AnyObject]!) {
+        
     }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    func transformStringToCBUUID(stringToBeTrans: String) -> CBUUID!{
+        let returnCBUUID:CBUUID = CBUUID.init(string: stringToBeTrans)
+        return returnCBUUID
     }
 
 
--- Mr_2C/ViewController.swift
+++ Mr_2C/ViewController.swift
@@ -7,9 +7,20 @@
 //
 
 import UIKit
+import CoreBluetooth
+
+let userName:String = "Leff's"
+//设备的唯一识别码
+let deviceUUID = UIDevice.currentDevice().identifierForVendor
+//服务的UUID
+let kServiceUUID:String = "C4FB2349-72FE-4CA2-94D6-1F3CB16222AA"
+//特征的UUID
+let kCharacteristicUUID_1:String = "6A3E4B28-522D-4B3B-82A9-D5E2004534FA"
+let kCharacteristicUUID_2:String = "6A3E4B28-522D-4B3B-82A9-D5E2004534FB"
+let kCharacteristicUUID_3:String = "6A3E4B28-522D-4B3B-82A9-D5E2004534FC"
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +30,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
